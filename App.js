f13f841dbfed46ce114071152a2eee5d94902d07
LassiVaisanen <t1vala02@students.oamk.fi>
2023-10-23 04:22:59 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,136 @@
+/*
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { MESSAGES, addDoc, collection, firestore } from './firebase/Config';
+import { useState } from 'react';
+
+export default function App() {
+  const [newMessage, setnewMessage] = useState("")
+
+  const save = async() => {
+    const docRef = await addDoc(collection(firestore, MESSAGES), {
+      text: newMessage,
+      created: serverTimestamp()
+    })
+    setnewMessage("")
+    console.log("Message saved")
+  }
+
+  return (
+    <View style={styles.container}>
+      <TextInput placeholder='Enter message' value = {newMessage} onChangeText={text => setnewMessage(text)}></TextInput>
+      <Button title="Save" onPress={save}></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+
+*/
+
+
+
+
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { Button, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+
+import { firestore, query, onSnapshot, collection, MESSAGES, addDoc, serverTimestamp, orderBy } from './firebase/Config';
+import Constants from 'expo-constants';
+
+import { convertFirebaseTimeStampToJS } from './helpers/Functions'
+
+
+export default function App() {
+  const [messages, setMessages] = useState([])
+  const [newMessage, setnewMessage] = useState("")
+
+  useEffect(() => {
+    const q = query(collection(firestore, MESSAGES), orderBy('created', 'desc'))
+
+    const unsubscribe = onSnapshot(q,(querySnapshot) => {
+      const tempMessages = []
+
+      querySnapshot.forEach((doc) => {
+
+        const messageObject = {
+          id: doc.id,
+          text: doc.data().text,
+          created: convertFirebaseTimeStampToJS(doc.data().created)
+        }
+        tempMessages.push(messageObject)
+      })
+      setMessages(tempMessages)
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+
+  const save = async() => {
+    const docRef = await addDoc(collection(firestore, MESSAGES), {
+      text: newMessage,
+      created: serverTimestamp()
+    }).catch (error => console.log(error))
+    setnewMessage("")
+  }
+  
+
+  return(
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        {
+          messages.map((message) => (
+            <View style={styles.message} key={message.id}>
+              <Text style={styles.messageInfo}>{message.created}</Text>
+              <Text>{message.text}</Text>
+            </View>
+          ))
+        }
+      </ScrollView>
+      
+      <View>
+        <TextInput placeholder='Enter message' value = {newMessage} onChangeText={text => setnewMessage(text)}></TextInput>
+        <Button title="Save" onPress={save}></Button>
+      </View>
+    </SafeAreaView>
+  )
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  message: {
+    padding: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: '#f5f5f5',
+    borderColor: '#ccc',
+    borderWidth: 1, borderRadius: 5, 
+    marginLeft: 10,
+    marginRight: 10
+  },
+  messageInfo: {
+    fontSize: 12,
+  },
+  inputContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'bottom',
+    justifyContent: 'bottom',
+  }
+});
